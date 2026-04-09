bc95b73b1049c3964f99b8e1a1412826c78a2d57
sabih-haider1 <sabih0364@gmail.com>
2026-04-09 22:27:28 +0500

    fixed the whitepaperes y spacing

--- src/components/pages/whitepapers/WhitepaperSearchBar.tsx
+++ src/components/pages/whitepapers/WhitepaperSearchBar.tsx
@@ -71,7 +71,7 @@ export function WhitepaperSearchBar({
   };
 
   return (
-    <div className="bg-[#F6F7FB] py-16 px-6 md:px-12 xl:px-20">
+    <div className="bg-[#F6F7FB] py-6 px-4 md:px-12 xl:px-20">
       <div className="max-w-7xl mx-auto">
         {/* Header Row */}
         <div className="flex flex-col justify-center items-center text-center md:flex-row md:justify-between md:text-left flex-wrap gap-4 mb-6">
